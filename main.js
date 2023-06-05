ec1ca5680f202b40aeb8995b77f845fc4b44cebd
Muccachino <lucas_staszewski@yahoo.de>
2023-06-05 13:53:42 +0200

    started adding color choice

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 "use strict";
 
 const mainBox = document.querySelector("#drawpad");
-mainBox.style.width = "800px";
-const resetButton = document.querySelector("#reset");
+mainBox.style.width = "700px";
 
 const gridCreator = (choice) => {
   mainBox.style.display = "grid";
@@ -13,15 +12,15 @@ const gridCreator = (choice) => {
 const boxCreator = (choice) => {
   for (let i = 0; i < choice * choice; i++) {
     let minibox = document.createElement("div");
-    minibox.style.width = 800 / choice + "px";
-    minibox.style.height = 800 / choice + "px";
+    minibox.style.width = 700 / choice + "px";
+    minibox.style.height = 700 / choice + "px";
     minibox.style.border = "1px solid black";
     minibox.className = "minibox";
     mainBox.appendChild(minibox);
   }
 };
 
-const colorChange = () => {
+const colorChange = (color) => {
   const allMinibox = document.querySelectorAll(".minibox");
 
   allMinibox.forEach((box) => {
@@ -39,10 +38,23 @@ const colorChange = () => {
 };
 
 const reset = () => {
-  let choice = prompt("How many colums and rows do you want?");
+  let choice = 0;
+  choice = parseInt(prompt("How many colums and rows do you want? (1 - 100)"));
+  while (choice > 100 || choice < 1) {
+    choice = parseInt(prompt("Please select a number between 1 and 100!"));
+  }
   mainBox.innerHTML = "";
   gridCreator(choice);
   boxCreator(choice);
   colorChange();
 };
+
+const resetButton = document.querySelector("#reset");
+const redButton = document.querySelector("#red");
+const greenButton = document.querySelector("green");
+const randomButton = document.querySelector("#randomColor");
+
 resetButton.addEventListener("click", reset);
+redButton.addEventListener("click", colorChange.bind(null, "red"));
+greenButton.addEventListener("click", colorChange.bind(null, "green"));
+randomButton.addEventListener("click", colorChange.bind(null, "random"));
