7a04de7db2d1963da659a6c4fd04982b1d48a054
Muccachino <lucas_staszewski@yahoo.de>
2023-06-06 08:36:36 +0200

    you can now choose between the colors red, blue, green and random

--- main.js
+++ main.js
@@ -22,22 +22,60 @@ const boxCreator = (choice) => {
 
 const colorChange = (color) => {
   const allMinibox = document.querySelectorAll(".minibox");
-
-  allMinibox.forEach((box) => {
-    box.addEventListener("mouseover", () => {
-      box.style.backgroundColor =
-        "rgb(" +
-        parseInt(Math.random() * 255) +
-        "," +
-        parseInt(Math.random() * 255) +
-        "," +
-        parseInt(Math.random() * 255) +
-        ")";
+  if (color === "red") {
+    redButton.style.border = "3px solid black";
+    greenButton.style.border = "none";
+    blueButton.style.border = "none";
+    randomButton.style.border = "none";
+    allMinibox.forEach((box) => {
+      box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = "red";
+      });
     });
-  });
+  } else if (color === "green") {
+    redButton.style.border = "none";
+    greenButton.style.border = "3px solid black";
+    blueButton.style.border = "none";
+    randomButton.style.border = "none";
+    allMinibox.forEach((box) => {
+      box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = "green";
+      });
+    });
+  } else if (color === "blue") {
+    redButton.style.border = "none";
+    greenButton.style.border = "none";
+    blueButton.style.border = "3px solid black";
+    randomButton.style.border = "none";
+    allMinibox.forEach((box) => {
+      box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = "blue";
+      });
+    });
+  } else if (color === "random") {
+    redButton.style.border = "none";
+    greenButton.style.border = "none";
+    blueButton.style.border = "none";
+    randomButton.style.border = "3px solid black";
+    allMinibox.forEach((box) => {
+      box.addEventListener("mouseover", () => {
+        box.style.backgroundColor =
+          "rgb(" +
+          parseInt(Math.random() * 255) +
+          "," +
+          parseInt(Math.random() * 255) +
+          "," +
+          parseInt(Math.random() * 255) +
+          ")";
+      });
+    });
+  }
 };
 
 const reset = () => {
+  redButton.style.border = "none";
+  greenButton.style.border = "none";
+  randomButton.style.border = "none";
   let choice = 0;
   choice = parseInt(prompt("How many colums and rows do you want? (1 - 100)"));
   while (choice > 100 || choice < 1) {
@@ -46,15 +84,16 @@ const reset = () => {
   mainBox.innerHTML = "";
   gridCreator(choice);
   boxCreator(choice);
-  colorChange();
 };
 
 const resetButton = document.querySelector("#reset");
 const redButton = document.querySelector("#red");
-const greenButton = document.querySelector("green");
+const greenButton = document.querySelector("#green");
+const blueButton = document.querySelector("#blue");
 const randomButton = document.querySelector("#randomColor");
 
 resetButton.addEventListener("click", reset);
 redButton.addEventListener("click", colorChange.bind(null, "red"));
 greenButton.addEventListener("click", colorChange.bind(null, "green"));
+blueButton.addEventListener("click", colorChange.bind(null, "blue"));
 randomButton.addEventListener("click", colorChange.bind(null, "random"));
